0e49dabe6d05fa2f897c1512e9f02d86b22afa1f
Hans-Peter Martini <hapemartini@gmail.com>
2020-12-28 18:22:42 +0100

    init and add the firebase library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(
+      environment.firebase,
+      'ready'
+    ),
     BrowserModule,
     AppRoutingModule
   ],
