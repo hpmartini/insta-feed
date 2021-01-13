4792127006e5b66fbdd48b76d241eee37ec12556
Hans-Peter Martini <hapemartini@gmail.com>
2021-01-13 18:42:23 +0100

    - split text into lines and iterate over each line - refactor animation using promises to enforce sequential processing of the input - add prettier to dependencies

--- src/app/row-runner/row-runner.component.ts
+++ src/app/row-runner/row-runner.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {AngularFireFunctions} from '@angular/fire/functions';
-import {Article} from '../model/article';
-import {FeedService} from '../services/feed.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { Article } from '../model/article';
+import { FeedService } from '../services/feed.service';
 
 @Component({
   selector: 'app-row-runner',
@@ -9,6 +9,8 @@ import {FeedService} from '../services/feed.service';
   styleUrls: ['./row-runner.component.sass'],
 })
 export class RowRunnerComponent implements OnInit {
+  @ViewChild('dummyDiv', { static: false }) dummyDiv: ElementRef;
+
   private url = 'https://rss.sueddeutsche.de/rss/Topthemen';
   private speed = 20;
 
@@ -16,8 +18,10 @@ export class RowRunnerComponent implements OnInit {
   public article: Article;
   public textBuffer: string;
 
-  constructor(private readonly functions: AngularFireFunctions, private readonly feedService: FeedService) {
-  }
+  constructor(
+    private readonly functions: AngularFireFunctions,
+    private readonly feedService: FeedService
+  ) {}
 
   ngOnInit(): void {
     this.feedService.article.subscribe((article) => {
@@ -37,23 +41,59 @@ export class RowRunnerComponent implements OnInit {
     this.isRowRunnerActive = true;
     this.textBuffer = '';
 
-    this.animate();
+    const lines = this.splitIntoLines(
+      this.dummyDiv.nativeElement,
+      this.article.content
+    );
+    this.animate(lines);
   }
 
-  /**
-   * for each char in string
-   *    if content is longer then div
-   *      try to split last word
-   *      add line to line array
-   */
-  private animate(): void {
-    [...this.article.content].forEach(async (char, index) => {
-      this.textBuffer = this.textBuffer?.slice(0, -2);
-      this.textBuffer += char;
-      this.textBuffer += ' ▶';
-      await this.sleep(index * this.speed);
+  private splitIntoLines(element, content): string[] {
+    const lines: string[] = [];
+
+    content.split(' ').reduce((pref: string, current: string) => {
+      element.innerText = this.concatString(pref, element.innerText, current);
+
+      if (element.scrollWidth > element.clientWidth) {
+        lines.push(element.innerText);
+        element.innerText = '';
+        return current; // todo hyphenate and check again
+      }
+      return '';
+    }, '');
+    element.innerText = '';
+
+    return lines;
+  }
+
+  private concatString(prev: string, dummy: string, current: string): string {
+    if (prev) {
+      return prev.concat(' ', dummy, ' ', current);
+    } else {
+      return dummy.concat(' ', current);
+    }
+  }
+
+  private async animate(content: string[]): Promise<void> {
+    for (const line of [...content]) {
+      await this.animateLine(line);
+    }
+  }
+
+  private async animateLine(line: string): Promise<void> {
+    for (const char of [...line]) {
+      await this.placeCharacter(char);
+    }
+  }
+
+  private placeCharacter(char): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.textBuffer = this.textBuffer?.slice(0, -2);
+        this.textBuffer += char;
+        this.textBuffer += ' ▉';
+        resolve();
+      }, this.speed);
     });
   }
-
-  sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 }
--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -1,27 +1,30 @@
-import {Injectable} from '@angular/core';
-import {AngularFireFunctions} from '@angular/fire/functions';
-import {Article} from '../model/article';
-import {BehaviorSubject} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { Article } from '../model/article';
+import { BehaviorSubject } from 'rxjs';
 
 const Readability = require('@mozilla/readability');
 
 @Injectable()
 export class FeedService {
-  private emptyArticle: {siteName: '', content: '', excerpt: '', title: ''};
+  private emptyArticle: { siteName: ''; content: ''; excerpt: ''; title: '' };
   public article = new BehaviorSubject<Article>(null);
 
-  constructor(private readonly functions: AngularFireFunctions) {
-  }
+  constructor(private readonly functions: AngularFireFunctions) {}
 
   public getArticleByUrl(url: string): void {
     this.article.next(this.emptyArticle);
     const getFeed = this.functions.httpsCallable('getFeed');
-    getFeed({url}).subscribe((result) => this.loadArticle(result.items[0].link));
+    getFeed({ url }).subscribe((result) =>
+      this.loadArticle(result.items[0].link)
+    );
   }
 
   private loadArticle(url): void {
     const getArticle = this.functions.httpsCallable('getArticle');
-    getArticle({url}).subscribe((article) => this.article.next(this.getParsedArticleWebSite(article)));
+    getArticle({ url }).subscribe((article) =>
+      this.article.next(this.getParsedArticleWebSite(article))
+    );
   }
 
   private getParsedArticleWebSite(result): Article {
