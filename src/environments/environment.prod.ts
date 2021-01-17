efc38bb56c63ab98756909fc09fe7635ec08ff94
Hans-Peter Martini <hapemartini@gmail.com>
2021-01-17 18:41:00 +0100

    - add firebase config to environment - add some todos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // todo regenerate keys and parameterize them with env vars on GitHub
+  firebase: {
+    apiKey: 'AIzaSyBcImIk9eIHXepMI5fRDK4IGSkuIVv2vaQ',
+    authDomain: 'ready-e9b4e.firebaseapp.com',
+    projectId: 'ready-e9b4e',
+    storageBucket: 'ready-e9b4e.appspot.com',
+    messagingSenderId: '790399774402',
+    appId: '1:790399774402:web:116a757e577fc8960c955b',
+    measurementId: 'G-GYD0T1QMZJ',
+  },
 };
