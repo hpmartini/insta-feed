0626bddeba0d084e3bb98b33eac711b5f5262da8
Hans-Peter Martini <hapemartini@gmail.com>
2021-01-17 11:53:14 +0100

    - remove unused firefunctions - remove unused packages - add better description to algorithm

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,42 +1,14 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
-import * as https from 'https';
 import * as Parser from 'rss-parser';
-const fetch = require('node-fetch');
 
-const Readability = require('@mozilla/readability');
-const jsdom = require('jsdom').JSDOM;
+const fetch = require('node-fetch');
 
 const app = express();
 const parser = new Parser();
 
 app.use(cors({ origin: true }));
-
-app.get('/:id', (req, res) => res.send(req.params.id));
-app.post('/', (req, res) =>
-  parser.parseURL(req.body.url).then((result: any) =>
-    https.get(result.items[0].link, (incomingMessage) => {
-      let body = '';
-      incomingMessage.on('data', (data) => (body += data));
-      return incomingMessage.on('end', () => {
-        const doc = new jsdom(body, result.items[0].link);
-        const reader = new Readability(doc.window.document);
-        const article = reader.parse();
-        res.send({
-          url: result.items[0].link,
-          title: article.title,
-          excerpt: article.excerpt,
-          content: article.content,
-          textContent: article.textContent,
-          siteName: article.siteName,
-        });
-      });
-    })
-  )
-);
-
-exports.loadFeed = functions.https.onRequest(app);
 
 exports.getFeed = functions.https.onCall(
   async (data) => await parser.parseURL(data.url)
--- src/app/services/text.service.ts
+++ src/app/services/text.service.ts
@@ -7,28 +7,32 @@ export class TextService {
   splitIntoLines(element, text): string[] {
     const lines: string[] = [];
 
+    // iterate over each word in the text and
+    // add them into lines according to the length
+    // of the reference element
     text.split(' ').reduce((pref: string, current: string) => {
       element.innerText = this.concatString(pref, element.innerText, current);
 
+      // when end of line is reached
       if (this.isEndOFLineReached(element)) {
-        // add text to result array when end of line is reached
+        // add text to result array
         lines.push(element.innerText);
+        // reset dummy element text
         element.innerText = '';
 
+        // return current word to be used in the next lind
         return current; // todo hyphenate and check again
       }
 
+      // return an empty string to go ahead
+      // with the next word in the current line
       return '';
     });
 
-    // reset dummy element's inner text
+    // reset dummy element text
     element.innerText = '';
 
     return lines;
-  }
-
-  private isEndOFLineReached(element): boolean {
-    return element.scrollWidth > element.clientWidth;
   }
 
   private concatString(prev: string, dummy: string, current: string): string {
@@ -37,5 +41,9 @@ export class TextService {
     } else {
       return dummy.concat(' ', current);
     }
+  }
+
+  private isEndOFLineReached(element): boolean {
+    return element.scrollWidth > element.clientWidth;
   }
 }
