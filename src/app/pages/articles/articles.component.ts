6fc02cafbaf4ec42c57aa9f33af979d1ee558b56
Hans-Peter Martini <hapemartini@gmail.com>
2021-04-05 16:31:56 +0200

    - remove breakpoint service again - some refactoring - move article API calls to own service

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FeedService } from '../../services/feed.service';
 import { ActivatedRoute } from '@angular/router';
 import { FeedObject } from '../../model/FeedObject';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-articles',
@@ -12,17 +12,17 @@ export class ArticlesComponent implements OnInit {
   public articleList: FeedObject[];
 
   constructor(
-    private readonly feedService: FeedService,
+    private readonly articleService: ArticleService,
     private readonly route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.feedService.articleList.subscribe((articles) => {
+    this.articleService.articleList.subscribe((articles) => {
       return (this.articleList = articles ?? null);
     });
     this.route.paramMap.subscribe((route) => {
       const feedUrl = 'https://'.concat(route.get('url'));
-      this.feedService.loadArticleList(feedUrl);
+      this.articleService.loadArticleList(feedUrl);
     });
   }
 }
