27639fbd0e6fbc5cfdfb0f555344d403341de5c9
Hans-Peter Martini <hapemartini@gmail.com>
2021-04-05 16:09:02 +0200

    - close drawer on link click in mobile view - fix some responsiveness issues in mobile and tablet view - introduce service to get breakpoints for tablet and mobile view

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,50 +1,41 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AnimationActiveService } from '../services/animation-active.service';
 import { Feed } from '../model/feed';
 import { FeedService } from '../services/feed.service';
 import { NavigationStart, Router } from '@angular/router';
+import { BreakpointService } from '../services/breakpoint.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.sass'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   public navEntries: Feed[];
   public isEditMode = false;
   public isAddMode = false;
   public isNavEntriesLoaded = false;
   public isHome = false;
   public isFeedListUpdating = false;
-
+  public isHandset$;
+  public isTablet$;
+  public currentPage: string;
   public newFeedForm = new FormGroup({
     name: new FormControl(''),
     url: new FormControl(''),
   });
-  public isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-  public isTablet$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Tablet)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-  public currentPage: string;
 
   constructor(
-    private readonly breakpointObserver: BreakpointObserver,
     private readonly feedService: FeedService,
     private readonly router: Router,
-    public readonly animationActiveService: AnimationActiveService
-  ) {
+    public readonly animationActiveService: AnimationActiveService,
+    public readonly breakpointService: BreakpointService
+  ) {}
+
+  ngOnInit(): void {
+    this.isHandset$ = this.breakpointService.isHandset$;
+    this.isTablet$ = this.breakpointService.isTablet$;
     this.feedService.feedList.subscribe((feedList) => {
       if (feedList?.length > 0) {
         this.navEntries = feedList;
