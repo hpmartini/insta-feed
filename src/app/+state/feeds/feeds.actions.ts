c8ebdc4cb414dab8fc74bd839c26c5b832d6d9ad
Hans-Peter Martini <hapemartini@gmail.com>
2021-04-07 22:40:14 +0200

    WIP

--- src/app/+state/feeds/feeds.actions.ts
+++ src/app/+state/feeds/feeds.actions.ts
@@ -5,7 +5,22 @@ const LOAD_FEEDS = '[Facade] LoadFeeds';
 const LOAD_FEEDS_SUCCESS = '[Facade] LoadFeeds Success';
 const LOAD_FEEDS_ERROR = '[Facade] LoadFeeds Failure';
 
+const ADD_FEED = '[Facade] AddFeed';
+const ADD_FEED_SUCCESS = '[Facade] AddFeed Success';
+const ADD_FEED_ERROR = '[Facade] AddFeed Failure';
+
+const DELETE_FEED = '[Facade] DeleteFeed';
+const DELETE_FEED_SUCCESS = '[Facade] DeleteFeed Success';
+const DELETE_FEED_ERROR = '[Facade] DeleteFeed Failure';
+
+const UPDATE_FEED = '[Facade] UpdateFeed';
+const UPDATE_FEED_SUCCESS = '[Facade] UpdateFeed Success';
+const UPDATE_FEED_ERROR = '[Facade] UpdateFeed Failure';
+
 export const loadFeeds = createAction(LOAD_FEEDS);
+export const addFeed = createAction(ADD_FEED);
+export const deleteFeed = createAction(DELETE_FEED);
+export const updateFeed = createAction(UPDATE_FEED);
 
 export const loadFeedsSuccess = createAction(
   LOAD_FEEDS_SUCCESS,
@@ -14,5 +29,35 @@ export const loadFeedsSuccess = createAction(
 
 export const loadFeedsFailure = createAction(
   LOAD_FEEDS_ERROR,
+  props<{ error: string }>()
+);
+
+export const addFeedSuccess = createAction(
+  ADD_FEED_SUCCESS,
+  props<{ feeds: Feed }>()
+);
+
+export const addFeedFailure = createAction(
+  ADD_FEED_ERROR,
+  props<{ error: string }>()
+);
+
+export const deleteFeedSuccess = createAction(
+  DELETE_FEED_SUCCESS,
+  props<{ feeds: Feed }>()
+);
+
+export const deleteFeedFailure = createAction(
+  DELETE_FEED_ERROR,
+  props<{ error: string }>()
+);
+
+export const updateFeedSuccess = createAction(
+  UPDATE_FEED_SUCCESS,
+  props<{ feeds: Feed }>()
+);
+
+export const updateFeedFailure = createAction(
+  UPDATE_FEED_ERROR,
   props<{ error: string }>()
 );
