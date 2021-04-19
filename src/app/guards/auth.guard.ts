028c42abe94f63c87271ea642a13948511d7bf3e
Hans-Peter Martini <hapemartini@gmail.com>
2021-04-19 20:17:38 +0200

    - make login, registration and logout work - add auth guard - hide main navigation when logged out

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,23 +2,36 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
-  UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { map, take, tap } from 'rxjs/operators';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return undefined;
+  ): Observable<boolean> {
+    return this.authService.user$?.pipe(
+      take(1),
+      map((user: User) => !!user),
+      tap((loggedIn: boolean) => {
+        if (!loggedIn) {
+          console.log('access denied');
+          this.router.navigate(['/']);
+        }
+      })
+    );
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,47 +1,72 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { FIREBASE_CONSTANTS } from '../constants/firebase.constants';
 import { User } from '../model/user';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  user$: Observable<User>;
+
   constructor(
     private readonly fireAuth: AngularFireAuth,
-    private readonly firestore: AngularFirestore
-  ) {}
-
-  loginWithGoogle(): Promise<void> {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    provider.setCustomParameters({
-      login_hint: 'user@example.com',
-    });
-    return this.fireAuth
-      .signInWithPopup(provider)
-      .then((response) => this.resolveResponse(response.user))
-      .catch((error) => window.alert(error));
+    private readonly firestore: AngularFirestore,
+    private readonly router: Router
+  ) {
+    this.user$ = this.fireAuth.authState.pipe(
+      switchMap((user: User) => this.getUserObservable(user))
+    );
   }
 
-  private resolveResponse({
+  private getUserObservable(user: User): Observable<any> {
+    return user
+      ? this.firestore.doc<User>(this.buildUserPath(user.uid)).valueChanges()
+      : of(null);
+  }
+
+  async loginWithGoogle(): Promise<void> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credentials = await this.fireAuth.signInWithPopup(provider);
+    return this.updateUserData(credentials.user);
+  }
+
+  async logOut(): Promise<boolean> {
+    console.log('logging out');
+    await this.fireAuth.signOut();
+    localStorage.removeItem('user');
+    return this.router.navigate(['/']);
+  }
+
+  private updateUserData({
     uid,
     email,
     displayName,
     emailVerified,
     photoURL,
   }: User): Promise<void> {
-    return this.firestore.collection(FIREBASE_CONSTANTS.users).doc(uid).set(
-      {
-        uid,
-        email,
-        displayName,
-        photoURL,
-        emailVerified,
-      },
-      { merge: true }
+    const userRef: AngularFirestoreDocument<User> = this.firestore.doc(
+      this.buildUserPath(uid)
     );
+    const data: User = {
+      uid,
+      email,
+      displayName,
+      emailVerified,
+      photoURL,
+    };
+    return userRef.set(data, { merge: true });
+  }
+
+  private buildUserPath(uid): string {
+    return FIREBASE_CONSTANTS.users.concat('/', uid);
   }
 }
